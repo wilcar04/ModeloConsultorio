b5d888d10bf8d56cf45359d5b9213135bd863ac4
Wilson <wilson.santiago.car@gmail.com>
2022-11-01 02:00:55 -0500

    Organización de código y adición de pistas de tipo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional, Type
 
 class Historial(ABC):
 
-    def __init__(self, fecha, informacion):
+    def __init__(self, fecha: datetime.date, informacion: str):
         self.fecha: datetime.date = fecha
         self.informacion: str = informacion
 
@@ -14,9 +14,19 @@ class Historial(ABC):
         pass
 
 
+class HistoriaClinica(Historial):
+
+    def __init__(self, fecha: datetime.date, informacion: str):
+        super().__init__(fecha, informacion)
+
+    def __str__(self):
+        return f"Fecha: {self.fecha}\n" \
+               f"Resultado: {self.informacion}"
+
+
 class ResultadoEcografia(Historial):
 
-    def __init__(self, fecha, informacion, tipo_ecografia):
+    def __init__(self, fecha: datetime.date, informacion: str, tipo_ecografia: str):
         super().__init__(fecha, informacion)
         self.tipo_ecografia: str = tipo_ecografia
 
@@ -26,73 +36,63 @@ class ResultadoEcografia(Historial):
                f"Resultado: {self.informacion}"
 
 
-class HistoriaClinica(Historial):
-
-    def __init__(self, fecha, informacion):
-        super().__init__(fecha, informacion)
-
-    def __str__(self):
-        return f"Fecha: {self.fecha}\n" \
-               f"Resultado: {self.informacion}"
-
-
 class Cita(ABC):
 
-    def __init__(self, fecha, hora, cedula_paciente):
+    def __init__(self, fecha: datetime.date, hora: datetime.time, cedula_paciente: str):
         self.fecha: datetime.date = fecha
         self.hora: datetime.time = hora
         self.cedula_paciente: str = cedula_paciente
         self.confirmada: bool = False
         self.atendida: bool = False
 
+    @abstractmethod
+    def __str__(self):
+        pass
+
+    @abstractmethod
+    def convertir_archivo(self, ruta_archivo: str) -> Historial:
+        pass
+
     def cita_confirmada(self):
         self.confirmada = True
-
-    def obtener_fecha_hora(self) -> tuple[datetime.date, datetime.time]:
-        return self.fecha, self.hora
 
     def cita_atendida(self):
         self.atendida = True
 
-    @abstractmethod
-    def convertir_archivo(self, archivo):
-        pass
+    def obtener_fecha_hora(self) -> tuple[datetime.date, datetime.time]:
+        return self.fecha, self.hora
 
-    @abstractmethod
+
+class CitaMedica(Cita):
+
+    def __init__(self, fecha: datetime.date, hora: datetime.time, cedula_paciente: str):
+        super().__init__(fecha, hora, cedula_paciente)
+
     def __str__(self):
+        return f"Fecha: {self.fecha}, Hora: {self.hora}"
+
+    def convertir_archivo(self, ruta_archivo: str) -> Historial:
         pass
 
 
 class CitaEcografia(Cita):
 
-    def __init__(self, fecha, hora, cedula_paciente, tipo_ecografia):
+    def __init__(self, fecha: datetime.date, hora: datetime.time, cedula_paciente: str, tipo_ecografia: str):
         super().__init__(fecha, hora, cedula_paciente)
         self.tipo_ecografia: str = tipo_ecografia
-
-    def convertir_archivo(self, ruta_archivo):
-        pass
 
     def __str__(self):
         return f"Fecha: {self.fecha}, Hora: {self.hora}, Tipo de Ecografía: {self.tipo_ecografia}"
 
-
-class CitaMedica(Cita):
-
-    def __init__(self, fecha, hora, cedula_paciente):
-        super().__init__(fecha, hora, cedula_paciente)
-
-    def convertir_archivo(self, ruta_archivo):
+    def convertir_archivo(self, ruta_archivo: str) -> Historial:
         pass
-
-    def __str__(self):
-        return f"Fecha: {self.fecha}, Hora: {self.hora}"
 
 
 class Paciente:
 
-    def __init__(self, nombre: str, cedula: str, sexo: str, fecha_nacimiento: datetime.date, celular: str):
-        self.nombre: str = nombre
+    def __init__(self, cedula: str, nombre: str, sexo: str, fecha_nacimiento: datetime.date, celular: str):
         self.cedula: str = cedula
+        self.nombre: str = nombre
         self.sexo: str = sexo
         self.fecha_nacimiento: datetime.date = fecha_nacimiento
         self.edad: int = self.calcular_edad(fecha_nacimiento)
@@ -120,7 +120,7 @@ class Paciente:
     def eliminar_cita(self):
         self.cita = None
 
-    def convertir_archivo(self, ruta_archivo) -> Historial:
+    def convertir_archivo(self, ruta_archivo: str) -> Historial:
         return self.cita.convertir_archivo(ruta_archivo)
 
     def agregar_historial(self, historial: Historial):
@@ -141,7 +141,7 @@ class Paciente:
 
 class AgendaDiaria:
 
-    def __init__(self, fecha):
+    def __init__(self, fecha: datetime.date):
         self.fecha: datetime.date = fecha
         self.citas: dict[datetime.time, Cita] = {}
 
@@ -154,9 +154,10 @@ class AgendaDiaria:
         return lista_cedulas_con_citas
 
     def hora_disponible(self, hora: datetime.time) -> bool:
-        return not hora in self.citas
+        return hora not in self.citas
 
-    def agregar_cita(self, fecha, hora, cedula_paciente, tipo_ecografia):
+    def agregar_cita(self, fecha: datetime.date, hora: datetime.time,
+                     cedula_paciente: str, tipo_ecografia: Optional[str]):
         if tipo_ecografia is not None:
             cita = CitaEcografia(fecha, hora, cedula_paciente, tipo_ecografia)
         else:
@@ -175,9 +176,6 @@ class AgendaDiaria:
             horas.append(cita.hora)
         return horas
 
-    def lista_citas_sin_confirmar(self):
-        pass
-
 
 class Agenda:
 
@@ -190,7 +188,7 @@ class Agenda:
         else:
             return True
 
-    def agregar_cita(self, fecha, hora, cedula_paciente, tipo_ecografia):
+    def agregar_cita(self, fecha: datetime.date, hora: datetime.time, cedula_paciente: str, tipo_ecografia: str):
         if fecha not in self.agendas_diarias:
             self.agendas_diarias[fecha] = AgendaDiaria(fecha)
         self.agendas_diarias[fecha].agregar_cita(fecha, hora, cedula_paciente, tipo_ecografia)
@@ -212,17 +210,17 @@ class Agenda:
 
 
 class Consultorio:
-    HORA_INICIAL = 9
-    HORA_FINAL = 16
-    ECOGRAFIAS = ("fetal", "abdominal", "vias urinarias", "mamaria", "muscular",
-                  "cervical", "renal")
+    HORA_INICIAL: int = 9
+    HORA_FINAL: int = 16
+    ECOGRAFIAS: tuple[str] = ("fetal", "abdominal", "vias urinarias", "mamaria", "muscular",
+                              "cervical", "renal")
 
     def __init__(self):
         self.pacientes: dict[str, Paciente] = {}
         self.agenda: Agenda = Agenda()
 
     @staticmethod
-    def obtener_numero_mes(mes) -> Optional[int]:
+    def obtener_numero_mes(mes: str) -> Optional[int]:
         meses = ("enero", "febrero", "marzo", "arbil", "mayo", "junio", "julio",
                  "agosto", "septiembre", "octubre", "noviembre", "diciembre")
         if mes.lower() in meses:
@@ -231,17 +229,17 @@ class Consultorio:
             return None
 
     @staticmethod
-    def obtener_formato_fecha(mes, dia) -> datetime.date:
+    def obtener_formato_fecha(numero_mes: int, dia: int) -> datetime.date:
         date = datetime.date.today()
         year = int(date.strftime("%Y"))
-        return datetime.date(year, mes, dia)
+        return datetime.date(year, numero_mes, dia)
 
     @staticmethod
-    def obtener_formato_hora(hora) -> datetime.time:
+    def obtener_formato_hora(hora: int) -> datetime.time:
         return datetime.time(hora, 0)
 
     @staticmethod
-    def dia_valido(dia, numero_mes) -> bool:
+    def dia_valido(dia: int, numero_mes: int) -> bool:
         if numero_mes in [1, 3, 5, 7, 8, 10, 12]:
             dias_del_mes = 31
         elif numero_mes in [4, 6, 9, 11]:
@@ -250,7 +248,7 @@ class Consultorio:
             dias_del_mes = 28
         return 0 < dia <= dias_del_mes
 
-    def hora_valida(self, hora) -> bool:
+    def hora_valida(self, hora: int) -> bool:
         return self.HORA_INICIAL <= hora <= self.HORA_FINAL
 
     def usuario_existe(self, cedula: str) -> bool:
@@ -266,7 +264,8 @@ class Consultorio:
             informacion.append(tuple[str(paciente), str(cita)])
         return informacion
 
-    def organizar_agenda(self, lista_horas: list[datetime.time], lista_informacion: list[Type[tuple]]) -> tuple[Optional[Type[tuple]]]:
+    def organizar_agenda(self, lista_horas: list[datetime.time],
+                         lista_informacion: list[Type[tuple]]) -> tuple[Optional[Type[tuple]]]:
         agenda_organizada = []
         for hora in range(self.HORA_INICIAL, self.HORA_FINAL + 1):
             for hora_cita in lista_horas:
@@ -281,16 +280,16 @@ class Consultorio:
 
     # Requisitos de programa:
 
-    def registrar_ususario(self, nombre: str, cedula: str, sexo: str, fecha_nacimiento: str, celular: str):
+    def registrar_ususario(self, cedula: str, nombre: str,  sexo: str, fecha_nacimiento: str, celular: str):
         if not self.usuario_existe(cedula):
             formato_fecha = None
             try:
                 lista_fecha = fecha_nacimiento.split("/")
                 formato_fecha = datetime.date(int(lista_fecha[2]), int(lista_fecha[1]), int(lista_fecha[0]))
             except ValueError:
-                # Error: La fecha de nacimiento ingresada no es válida
                 pass
-            paciente = Paciente(nombre, cedula, sexo, formato_fecha, celular)
+                # Error: La fecha de nacimiento ingresada no es válida
+            paciente = Paciente(cedula, nombre, sexo, formato_fecha, celular)
             self.pacientes[cedula] = paciente
         else:
             # Error: Usuario ya registrado
@@ -333,7 +332,7 @@ class Consultorio:
             pass
         self.agenda.agregar_cita(formato_fecha, formato_hora, cedula, tipo_ecografia)
 
-    def confimar_cita(self, cedula):
+    def confimar_cita(self, cedula: str):
         if self.usuario_existe(cedula):
             paciente = self.pacientes[cedula]
             if paciente.paciente_tiene_cita():
@@ -381,7 +380,7 @@ class Consultorio:
         if not self.dia_valido(dia, numero_mes):
             # Error: El día ingresado no es válido
             pass
-        fecha = self.obtener_formato_fecha(mes, dia)
+        fecha = self.obtener_formato_fecha(numero_mes, dia)
         if not self.agenda.fecha_tiene_citas(fecha):
             # Error: La fecha ingresada no tiene citas
             pass
